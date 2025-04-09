9be8cbd5e599119e2a7bb232a05d4f681255a70b
Ryu Juheon <saidbysolo@gmail.com>
2025-04-09 10:25:09 +0900

    feat: 급식 엔드포인트 블루프린트 그룹에 추가

--- backend/adapters/controllers/endpoint/api/__init__.py
+++ backend/adapters/controllers/endpoint/api/__init__.py
@@ -2,9 +2,11 @@ from sanic.blueprints import Blueprint
 
 from backend.adapters.controllers.endpoint.api.user import user
 from backend.adapters.controllers.endpoint.api.school import school
+from backend.adapters.controllers.endpoint.api.meal import meal
 
 api_endpoint = Blueprint.group(
     user,
     school,
+    meal,
     url_prefix="/api",
 )
