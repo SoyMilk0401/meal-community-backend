dbfa541f682493cc2c63a6d282a4e3207b81b899
한푸른 <h1182820401@gmail.com>
2025-05-23 22:53:52 +0900

    댓글 엔드포인트 dto 추가가

--- backend/adapters/controllers/endpoint/api/comment.py
+++ backend/adapters/controllers/endpoint/api/comment.py
@@ -2,7 +2,7 @@ from sanic import json
 from sanic.blueprints import Blueprint
 from sanic_ext import validate
 
-from backend.application.dtos.comment import CreateCommentDTO
+from backend.application.dtos.comment import CreateCommentDTO, CreateReplyCommentDTO
 from backend.application.use_cases.create.comment import CreateNewCommentUseCase
 from backend.application.use_cases.create.comment import CreateReplyCommentUseCase
 from backend.domain.entities.comment import Comment
@@ -40,28 +40,26 @@ async def write_comment(
     return json({"message": "Comment created successfully"})
 
 @comment.post("/reply")
-@validate(json=CreateCommentDTO, body_argument="create_comment_dto")
+@validate(json=CreateReplyCommentDTO, body_argument="create_reply_comment_dto")
 @require_auth
 async def write_reply(
     request: BackendRequest,
-    create_comment_dto: CreateCommentDTO,
+    create_reply_comment_dto: CreateReplyCommentDTO,
     user_id: int,
 ):
-    if create_comment_dto.parent_id is None:
-        return json({"error": "Parent ID is required"})
 
     user = await GetUserByIDUseCase(request.app.ctx.user_repository).execute(
         user_id
     )
 
     comment_entity = Comment(
-        content=create_comment_dto.content,
+        content=create_reply_comment_dto.content,
         author=user,
-        parent_id=create_comment_dto.parent_id)
+        parent_id=create_reply_comment_dto.parent_id)
     
     await CreateReplyCommentUseCase(request.app.ctx.comment_repository).execute(
             user_id=user_id,
-            meal_id=create_comment_dto.meal_id, 
+            meal_id=create_reply_comment_dto.meal_id, 
             comment=comment_entity,
             parent_comment_id=comment_entity.parent_id
         )
