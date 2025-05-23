b1fdd204930af321677261be7d098ba0f586bed4
한푸른 <h1182820401@gmail.com>
2025-05-23 22:53:52 +0900

    댓글 엔드포인트 dto 추가

--- backend/application/dtos/comment.py
+++ backend/application/dtos/comment.py
@@ -5,7 +5,16 @@ from dataclasses import dataclass
 class CreateCommentDTO:
     meal_id: int
     """급식 ID"""
-    parent_id: int | None
-    """부모 댓글 ID"""
     content: str
     """댓글 내용"""
+    parent_id: int | None = None
+    """부모 댓글 ID"""
+    
+@dataclass
+class CreateReplyCommentDTO:
+    meal_id: int
+    """급식 ID"""
+    content: str
+    """댓글 내용"""
+    parent_id: int
+    """부모 댓글 ID"""
