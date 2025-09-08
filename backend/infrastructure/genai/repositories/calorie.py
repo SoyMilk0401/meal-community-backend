8a6ed36736f3e23392d4b0ff0a210f0d4b9086ca
Ryu Juheon <saidbysolo@gmail.com>
2025-09-08 15:01:20 +0900

    refactor: improved response handling

--- backend/infrastructure/genai/repositories/calorie.py
+++ backend/infrastructure/genai/repositories/calorie.py
@@ -1,34 +1,32 @@
 from io import IOBase
 from json import loads
 
+import pydantic
 from google.genai.client import AsyncClient
 
 from backend.domain.entities.calorie import Calorie, Menu
 from backend.domain.entities.meal import Meal
 from backend.domain.repositories.calorie import CalorieRepository
 
+
+class MenuResponse(pydantic.BaseModel):
+    name: str
+    calories: int | None
+
+
+class CalorieResponse(pydantic.BaseModel):
+    meals: list[MenuResponse]
+    total_calories: int
+
+
 PROMPT = """
 너는 전문 영양사야. [{food}] 대괄호 안에 담긴 메뉴는 식판에 담긴 예상 음식 목록이야.
-
-```json
-{{
-    "meals": [
-        {{
-            "name": "<name of the meal>",
-            "calories": "<total calories of the meal if not available, use null>",
-        }}
-    ],
-    "total_calories": "<total calories of all meals>",
-}}
-```
 
 실제 사진에 담긴 식판을 보고, 음식의 종류와 양을 추론해서 상단의 JSON 형식으로 결과를 반환해줘.
 
 최대한 칼로리와 음식의 종류를 정확하게 추론해줘.
 
-만약 그럼에도 불구하고 음식의 종류나 칼로리를 정확하게 알 수 없다면, 해당 값을 null로 설정해줘.
-
-내가 제공한 음식 목록과 예시를 참고해서 JSON만 작성해줘.
+만약 그럼에도 불구하고 음식의 종류나 칼로리를 정확하게 알 수 없고, 음식과 관련된 사진이 아니라면 null 또는 비어있는 응답을 반환해줘.
 """
 
 
@@ -46,16 +44,22 @@ class GeminiCalorieRepository(CalorieRepository):
         )
 
         response = await self.client.models.generate_content(  # pyright: ignore[reportUnknownMemberType]
-            model="gemini-2.0-flash",
+            model="gemini-2.5-flash",
             contents=[file, PROMPT.format(food=menus)],
+            config={
+                "response_mime_type": "application/json",
+                "response_schema": CalorieResponse,
+            },
         )
 
         if not response.text:
             return None
 
-        json = response.text.replace("```json", "").replace("```", "")
+        data = loads(response.text)
 
-        data = loads(json)
+        if not data["total_calories"]:
+            return None
+
         meals = [
             Menu(name=menu["name"], calories=menu["calories"]) for menu in data["meals"]
         ]
